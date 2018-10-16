05be83d5608c7944f787e767294eab61fe46113e
Justin_Tu <justintu@sbcglobal.net>
2018-10-15 20:27:51 -0400

    Changed pom.xml and UserDaoTest.java to use JUnit4

--- Spring code/src/test/java/com/revature/test/UserDaoTest.java
+++ Spring code/src/test/java/com/revature/test/UserDaoTest.java
@@ -1,8 +1,8 @@
 package com.revature.test;
 
 import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 
 import org.junit.Rule;
