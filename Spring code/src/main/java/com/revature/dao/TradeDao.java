e13a802c42f8f3fa90404f36fe7991e682bc6f0e
Justin_Tu <justintu@sbcglobal.net>
2018-10-16 19:33:25 -0400

    changes to some classes

--- Spring code/src/main/java/com/revature/dao/TradeDao.java
+++ Spring code/src/main/java/com/revature/dao/TradeDao.java
@@ -1,17 +1,9 @@
 package com.revature.dao;
 
-<<<<<<< HEAD
 import javax.persistence.RollbackException;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-=======
-import javax.persistence.OptimisticLockException;
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
->>>>>>> 739943b139bc016732f7d8ed72fbf3182384d75d
 import org.jboss.logging.Logger;
 
 import com.revature.entities.Trade;
@@ -27,10 +19,10 @@ public class TradeDao implements Dao<Trade> {
 	public Trade read(int id) {
 		return sess.get(Trade.class, id);
 	}
+	
 
 	@Override
 	public int create(Trade t) {
-<<<<<<< HEAD
 		try {
 			Transaction trans = sess.beginTransaction();
 			Integer generatedId = (Integer) sess.save(t);
@@ -40,17 +32,10 @@ public class TradeDao implements Dao<Trade> {
 			log.warn("Inside TradeDao.create(Trade)" + e.getMessage());
 			return -1;
 		}
-=======
-		Transaction trans = sess.beginTransaction();
-		sess.save(t);
-		trans.commit();
-		return t.getTradeId();
->>>>>>> 739943b139bc016732f7d8ed72fbf3182384d75d
 	}
 
 	@Override
 	public Trade update(int id, Trade t) {
-<<<<<<< HEAD
 		try {
 			Transaction trans = sess.beginTransaction();
 			Trade current = sess.get(Trade.class, id);
@@ -63,27 +48,6 @@ public class TradeDao implements Dao<Trade> {
 			return current;
 		} catch (RollbackException e) {
 			log.warn("Inside TradeDao.update(Trade)" + e.getMessage());
-=======
-		
-		Trade current = sess.get(Trade.class, id);
-		if(current != null) {
-			try {
-				Transaction trans = sess.beginTransaction();
-				current.setItemName(t.getItemName());
-				current.setItemQuantity(t.getItemQuantity());
-				current.setMessage(t.getMessage());
-				current.setOffer(t.isOffer());
-				sess.update(current);
-				trans.commit();
-				return current;
-			} catch (OptimisticLockException e) {
-				log.warn("Inside Trade.update(int, Trade) " + e.getMessage());
-				sess.getTransaction().rollback();
-				return null;
-			}
-			
-		} else {
->>>>>>> 739943b139bc016732f7d8ed72fbf3182384d75d
 			return null;
 		}
 	}
@@ -94,14 +58,13 @@ public class TradeDao implements Dao<Trade> {
 			Transaction trans = sess.beginTransaction();
 			sess.delete(t);
 			trans.commit();
-<<<<<<< HEAD
 		} catch (RollbackException e) {
-			
-=======
-		} catch (OptimisticLockException e) {
 			log.warn("Inside Trade.delete(Trade) " + e.getMessage());
 			sess.getTransaction().rollback();
->>>>>>> 739943b139bc016732f7d8ed72fbf3182384d75d
 		}
+	}
+	
+	public void test() {
+		
 	}
 }
